208b0313f575751ef3f7e1aa279054142b28b19a
Haydn Nitzsche <haydnnitzsche@gmail.com>
2020-07-17 19:15:52 -0700

    Change how globals are used and introduce basic CLI validation

--- manga_pdf_downloader.py
+++ manga_pdf_downloader.py
@@ -2,45 +2,93 @@
 #
 # Written by @HaydnNitzsche
 
-
 from lxml import html
 import requests
 import os
+import sys
 import os.path
 import argparse
 from os import path
 from reportlab.pdfgen.canvas import Canvas
 from reportlab.lib.utils import ImageReader
 
-WEBSITE_BASE_PATH = ""
+# Set up parser for CLI arguments
+PARSER = argparse.ArgumentParser(description="Download manga from kissmanga.com in pdf format", usage='%(prog)s url')
+PARSER.add_argument('url', help='Manga url. Must be a link from https://www.mngdoom.com/')
+
+MANGA_NAME = None
+def get_manga_name():
+    global MANGA_NAME
+    if not MANGA_NAME:
+        tree = get_base_path_tree()
+        MANGA_NAME = tree.xpath("//title/text()")[0].split(" Manga - ")[0].split("Read ")[1]
+    return MANGA_NAME
+
+MANGA_BASE_PATH = None
+def get_manga_base_path():
+    global MANGA_BASE_PATH
+    global PARSER
+    if not MANGA_BASE_PATH:
+        args = PARSER.parse_args()
+        MANGA_BASE_PATH = args.url
+    return MANGA_BASE_PATH
+
 BASE_PATH_TREE = None
+def get_base_path_tree():
+    global BASE_PATH_TREE
+    if not BASE_PATH_TREE:
+        webpage = requests.get(get_manga_base_path())
+        BASE_PATH_TREE = html.fromstring(webpage.content)
+    return BASE_PATH_TREE
 
 def main():
+    validate_inputs()
     setup()
     download_chapters()
-    
+
+def validate_inputs():
+    global PARSER
+    args = PARSER.parse_args()
+    if args.url.find("https://www.mngdoom.com/") is -1:
+        sys.exit("manga_pdf_downloader: error: invalid value for url. Must be a link from https://www.mngdoom.com/")
+
+def setup():
+    manga_chapters_path = "manga/%s"%(get_manga_name())
+    if not os.path.isdir(manga_chapters_path):
+        print("Output directory not found, creating it now at %s."%(get_manga_name()))
+        os.makedirs(manga_chapters_path)
+    disclaimer_file_path = "manga/disclaimer.txt"
+    if not os.path.isfile(disclaimer_file_path):
+        print("Disclaimer file not found, creating it now at %s."%(disclaimer_file_path))
+        disclaimer = open(disclaimer_file_path, "w+").write("Disclaimer: These chapters were downloaded from %s and are scanlations. The quality of typesetting/translation is not guaranteed to be great and, as such, you should buy the official releases if you want high quality translations." %(get_manga_base_path()))
+
 def download_chapters():
-    for chapter in get_chapters()[::-1]:
-        title = chapter.text.strip()
-        outfile_path = "manga/%s/%s.pdf" %(get_manga_name(), title)
+    for chapter in get_base_path_tree().findall(".//span[@class='val']")[::-1]:
+        chapter_title = chapter.text.strip()
+        outfile_path = "manga/%s/%s.pdf" %(get_manga_name(), chapter_title)
         if path.exists(outfile_path):
-            print("Skipping chapter: %s. Already downloaded." %(title))
+            print("Skipping chapter: %s. Already downloaded." %(chapter_title))
         else:
-            chapter_number = title.split(" - ")[-1]
-            current_webpage_path = build_chapter_path(chapter_number) 
+            chapter_number = chapter_title.split(" - ")[-1]
+            current_webpage_path = "%s/%s/all-pages" %(get_manga_base_path(), chapter_number)
             current_webpage = requests.get(current_webpage_path)
             tree = html.fromstring(current_webpage.content)
             img_urls = tree.xpath('//img/@src')
-            generate_pdf(title,img_urls,outfile_path)
+            generate_pdf(chapter_title,img_urls,outfile_path)
+    print("Finished downloading chapters for: %s"%(get_manga_name()))
 
 def generate_pdf(chapter_title, img_urls, out_file_path):
     print("Downloading images for chapter: %s."%(chapter_title))
     canvas = Canvas(out_file_path)
     canvas.setTitle(chapter_title)
     for img_url in img_urls:
+        # Grabs the download link starting from the last occurence of http.
+        # Helps solve some broken links
+        img_url = img_url[img_url.rfind("http"):]
         print ('downloading image: %s' % img_url)
         filename = img_url.split('/')[-1]
-        download_img(img_url, filename)
+        r = requests.get(img_url, allow_redirects=True)
+        open(filename, "wb").write(r.content)
         try:
             image = ImageReader(filename)
             canvas.setPageSize(image.getSize())
@@ -50,46 +98,6 @@ def generate_pdf(chapter_title, img_urls, out_file_path):
             print("Error, bad file %s downloaded from %s."%(filename,img_url))
         os.remove(filename)
     canvas.save()
-    print("Finished downloading images for chapter: %s."%(chapter_title))
-
-def setup():
-    global WEBSITE_BASE_PATH
-    parser = argparse.ArgumentParser(description="Download manga from kissmanga.com in pdf format", usage='%(prog)s url')
-    parser.add_argument('url', help='Manga url. Must be from https://www.mngdoom.com/.')
-    args = parser.parse_args()
-    WEBSITE_BASE_PATH = args.url
-    manga_name = get_manga_name()
-    disclaimer_file_path = "manga/disclaimer.txt"
-    manga_chapters_path = "manga/%s"%(manga_name)
-    if not os.path.isdir(manga_chapters_path):
-        print("Output directory not found, creating it now at %s."%(manga_name))
-        os.makedirs(manga_chapters_path)
-    if not os.path.isfile(disclaimer_file_path):
-        print("Disclaimer file not found, creating it now at %s."%(disclaimer_file_path))
-        disclaimer = open(disclaimer_file_path, "w+").write("Disclaimer: These chapters were downloaded from %s and are scanlations. The quality of typesetting/translation is not guaranteed to be great and, as such, you should buy the official releases if you want high quality translations." %(WEBSITE_BASE_PATH))
-
-def get_chapters():
-    tree = get_base_path_tree()
-    chapters = tree.findall(".//span[@class='val']")
-    return chapters
-
-def get_manga_name():
-    tree = get_base_path_tree()
-    return tree.xpath("//title/text()")[0].split(" Manga - ")[0].split("Read ")[1]
-
-def get_base_path_tree():
-    global BASE_PATH_TREE
-    if not BASE_PATH_TREE:
-        webpage = requests.get(WEBSITE_BASE_PATH)
-        BASE_PATH_TREE = html.fromstring(webpage.content)
-    return BASE_PATH_TREE
-
-def download_img(img_url, filename):
-    r = requests.get(img_url, allow_redirects=True)
-    downloaded_img = open(filename, "wb").write(r.content)
-
-def build_chapter_path(chapter_number):
-    global WEBSITE_BASE_PATH
-    return WEBSITE_BASE_PATH + "/%s/all-pages" %(chapter_number)
+    print("Finished downloading chapter: %s."%(chapter_title))
 
 main()
